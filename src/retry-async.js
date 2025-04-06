d2be3732c38998eea514c1f5bd65367060c7a2ea
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2025-04-06 14:38:20 +0100

    add docs

--- src/retry-async.js
+++ src/retry-async.js
@@ -1,11 +1,46 @@
 /**
+ * @type {object} RetryStatus
+ * Retry-status object type
+ *
+ * @param {number} RetryStatus.index
+ * Retry overall duration, in milliseconds
+ *
+ * @param {number} RetryStatus.duration
+ * Retry overall duration, in milliseconds.
+ *
+ * @param {any} [error]
+ * Last error, if available;
+ * It is undefined only when "retryAsync" calls "func" with index = 0.
+ */
+
+/**
+ * @function retryAsync
  * Retries async operation returned from "func" callback, according to "options".
+ *
+ * @param {(s:RetryStatus) => Promise} func
+ * Function that returns a Promise - result of the async operation to be retried.
+ *
+ * @param {object} [options]
+ * Optional Retry Options object.
+ *
+ * @param {number|((s:RetryStatus)=>number)} [options.retry]
+ * Maximum number of retries (infinite by default),
+ * or a callback to indicate the need for another retry.
+ *
+ * @param {number|((s:RetryStatus)=>number)} [options.delay]
+ * Retry delays, in milliseconds (no delay by default),
+ * or a callback that returns the delays.
+ *
+ * @param {(s:RetryStatus)=>void} [options.error]
+ * Error notifications.
+ *
+ * @returns {Promise}
  */
 function retryAsync(func, options) {
     const start = Date.now();
     let index = 0, e;
-    let { retry = Number.POSITIVE_INFINITY, delay = -1, error } = options ?? {};
-    const s = () => ({ index, duration: Date.now() - start, error: e });
+    let {retry = Number.POSITIVE_INFINITY, delay = -1, error} = options ?? {};
+    const s = () => ({index, duration: Date.now() - start, error: e});
     const c = () => func(s()).catch(err => {
         e = err;
         typeof error === 'function' && error(s());
@@ -18,3 +53,5 @@ function retryAsync(func, options) {
     });
     return c();
 }
+
+module.exports = {retryAsync};
